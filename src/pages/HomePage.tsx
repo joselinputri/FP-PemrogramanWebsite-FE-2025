763b4d3f21429c1cb8a87de03dfc503bdaaaab16
reva <reva.erica24@gmail.com>
2026-01-04 21:35:40 +0700

    Remove unused gameTemplates mapping in HomePage
    Removed the mapping of gameTemplates to DropdownMenuItems when no templates are available.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -557,19 +557,6 @@ export default function HomePage() {
                 ) : (
                   <DropdownMenuItem disabled>
                     No templates available
-                {gameTemplates?.map((template) => (
-                  <DropdownMenuItem
-                    key={template.id}
-                    onClick={() =>
-                      setGameTypeSlug(
-                        gameTypeSlug === template.slug ? null : template.slug,
-                      )
-                    }
-                    className={
-                      gameTypeSlug === template.slug ? "bg-sky-100" : ""
-                    }
-                  >
-                    {template.name}
                   </DropdownMenuItem>
                 )}
               </DropdownMenuContent>
